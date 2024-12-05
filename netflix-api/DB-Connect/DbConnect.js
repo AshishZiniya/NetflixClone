49f6732e1df638174ef3798446673e4b7c9af97b
Ziniya Ashish <Ashishziniya1234@gmail.com>
2024-12-05 14:22:10 +0530

    update

--- netflix-api/DB-Connect/DbConnect.js
+++ netflix-api/DB-Connect/DbConnect.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    }).then(() => console.log('Connected to the Database & Listening on port 3000'));
+    }).then(() => console.log('Connected to the Database & Listening on port',process.env.PORT));
   } catch (error) {
     console.error("Error connecting to MongoDB:", error.message);
     process.exit(1);
--- netflix-api/index.js
+++ netflix-api/index.js
@@ -2,7 +2,13 @@ const express = require("express");
 const cors = require("cors");
 const userRoutes = require("./Routes/UserRoutes");
 const connectDB = require("./DB-Connect/DbConnect");
+const {
+    addToLikedMovies,
+    getLikedMovies,
+    removeFromLikedMovies,
+} = require("./Controllers/UserController");
 require('dotenv').config();
+const router = require("express").Router();
 
 const app = express();
 
@@ -11,9 +17,13 @@ app.use(express.json());
 
 connectDB();
 
-app.use("/api/user", userRoutes);
+
+router.get("/liked/:email", getLikedMovies);
+router.post("/add", addToLikedMovies);
+router.put("/delete", removeFromLikedMovies);
+
+app.use("/api/user", router);
 
 app.listen(process.env.PORT, () => {
-    console.log('Connected to the Database & Listening on port', process.env.PORT);
     console.log('Server Started Successfully');
 });
